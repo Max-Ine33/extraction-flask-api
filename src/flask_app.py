e26fd24fe4bd8811a30bd0396473b077e5dcc0d1
Max-Ine33 <maxinegravier@gmail.com>
2023-12-26 14:49:19 +0100

    tested black and isort

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,42 +1,49 @@
 from flask import Flask, jsonify, request
+
 import arxiv_crawler
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return 'Homepage lol'
+    return "Homepage lol"
 
 
-@app.route('/articles', methods=['GET'])
+@app.route("/articles", methods=["GET"])
 def get_articles():
-    '''Get all articles of arXiv. Can search articles by keyword with --> articles?search=keyword'''
-    page = int(request.args.get('page', 1))
+    """Get all articles of arXiv. Can search articles by keyword with --> articles?search=keyword"""
+    page = int(request.args.get("page", 1))
     per_page = 10
     start = (page - 1) * per_page
     max_results = per_page
-    search = request.args.get('search')
+    search = request.args.get("search")
 
-    articles = arxiv_crawler.get_arxiv_articles(query=search, start=start, max_results=max_results)
+    articles = arxiv_crawler.get_arxiv_articles(
+        query=search, start=start, max_results=max_results
+    )
 
     if articles is not None:
-        return jsonify({'articles': articles})
+        return jsonify({"articles": articles})
     else:
-        return jsonify({'error': 'Failed to fetch articles from arXiv'}), 500
+        return jsonify({"error": "Failed to fetch articles from arXiv"}), 500
 
 
-@app.route('/articles/<path:id>', methods=['GET']) # Used <path:> because there is a / in the id of an article
+@app.route(
+    "/articles/<path:id>", methods=["GET"]
+)  # Used <path:> because there is a / in the id of an article
 def get_articles_by_id(id):
-    '''Get metadata on an article using its id --> /articles/id-example'''
+    """Get metadata on an article using its id --> /articles/id-example"""
     metadata = arxiv_crawler.fetch_metadata_by_id(id)
     return metadata
 
-@app.route('/text/<path:id>', methods=['GET'])
+
+@app.route("/text/<path:id>", methods=["GET"])
 def get_summary(id):
-    '''Get summary of an article using its id --> /text/id-example'''
+    """Get summary of an article using its id --> /text/id-example"""
     summary = arxiv_crawler.fetch_summary_by_id(id)
     return summary
 
 
-if __name__ == '__main__':
-    app.run(debug=True) #Reload app when changes are made
+if __name__ == "__main__":
+    app.run(debug=True)  # Reload app when changes are made
